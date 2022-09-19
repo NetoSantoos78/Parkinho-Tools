fc907c3b3a523f07f0b4aa211f4074f7502e7973
NetoSantoos78 <95303077+NetoSantoos78@users.noreply.github.com>
2022-09-19 10:27:44 -0300

    UPDATE

--- index.js
+++ index.js
@@ -5,30 +5,28 @@ import * as CryptoJS from 'crypto-js';
 
 const cfg = {
   mode: CryptoJS.mode.CBC,
-  padding: CryptoJS.pad.Pkcs7
+  padding: CryptoJS.pad.Pkcs7,
 };
- 
+
 class App extends Component {
   constructor() {
-    super(); 
+    super();
     this.state = {
       inputText: '',
       inputKey: '',
       encryptedBase64Input: '',
       encryptedBase64: '',
       decryptKey: '',
-      decryptedText: ''
+      decryptedText: '',
     };
-
   }
 
   /*
-  * Encrypt a derived hd private key with a given pin and return it in Base64 form
-  */
+   * Encrypt a derived hd private key with a given pin and return it in Base64 form
+   */
   encryptAES = (text, key) => {
     return CryptoJS.AES.encrypt(text, key).toString();
   };
-
 
   /**
    * Decrypt an encrypted message
@@ -46,7 +44,7 @@ class App extends Component {
           return str;
         } else {
           return 'error 1';
-        } 
+        }
       } catch (e) {
         return 'error 2';
       }
@@ -55,46 +53,65 @@ class App extends Component {
   };
 
   handleInputTextChange = (event) => {
-    this.setState({
-      inputText: event.target.value
-    }, this.encryptInputText)
-  }
+    this.setState(
+      {
+        inputText: event.target.value,
+      },
+      this.encryptInputText
+    );
+  };
 
   handleInputKeyChange = (event) => {
-    this.setState({
-      inputKey: event.target.value
-    }, this.encryptInputText)
-  }
+    this.setState(
+      {
+        inputKey: event.target.value,
+      },
+      this.encryptInputText
+    );
+  };
 
   encryptInputText = () => {
     this.setState({
-      encryptedBase64Input: this.encryptAES(this.state.inputText, this.state.inputKey)
-    })
-  }
+      encryptedBase64Input: this.encryptAES(
+        this.state.inputText,
+        this.state.inputKey
+      ),
+    });
+  };
 
   handleDecryptKeyChange = (event) => {
-    this.setState({
-      decryptKey: event.target.value
-    }, this.decryptOutputText)
-  }
+    this.setState(
+      {
+        decryptKey: event.target.value,
+      },
+      this.decryptOutputText
+    );
+  };
 
   handleMsgChange = (event) => {
-    this.setState({
-      encryptedBase64: event.target.value
-    }, this.decryptOutputText)
-  }
+    this.setState(
+      {
+        encryptedBase64: event.target.value,
+      },
+      this.decryptOutputText
+    );
+  };
 
   decryptOutputText = () => {
     this.setState({
-      outputText: this.decryptAES(this.state.encryptedBase64, this.state.decryptKey)
-    })
-  }
+      outputText: this.decryptAES(
+        this.state.encryptedBase64,
+        this.state.decryptKey
+      ),
+    });
+  };
 
   testForProblems = () => {
     for (const i = 0; i < 20000; i++) {
       setTimeout(() => {
         const key = '123';
-        const passphrase = 'this is a very long passphrase with a ton of words in it but it shouldnt really matter';
+        const passphrase =
+          'this is a very long passphrase with a ton of words in it but it shouldnt really matter';
         const encrypted = this.encryptAES(passphrase, key);
 
         setTimeout(() => {
@@ -104,28 +121,53 @@ class App extends Component {
         }, 1000 + i);
       }, 100 + i);
     }
-  }
+  };
 
   render() {
-    const {error, transactions, isLoading} = this.state;
-    if(error){
-      return <h3>{error}</h3>
+    const { error, transactions, isLoading } = this.state;
+    if (error) {
+      return <h3>{error}</h3>;
     }
     return (
       <>
         <h1>Encriptar o Texto !</h1>
-        <h2 style="display" >Key: </h2><h5>CMozpq0398190ijaVMiaefori666666</h5>
         <div className="form-group">
-          <input className="form-control" value={this.state.inputText} onChange={this.handleInputTextChange} style={{width:'40%', height:40, marginRight: 20}} placeholder="Input Text" /> 
-          <input className="form-control" value={this.state.inputKey} onChange={this.handleInputKeyChange} style={{width:'40%', height:40}} placeholder="Key" />
+          <input
+            className="form-control"
+            value={this.state.inputText}
+            onChange={this.handleInputTextChange}
+            style={{ width: '40%', height: 40, marginRight: 20 }}
+            placeholder="Input Text"
+          />
+          <input
+            className="form-control"
+            value={this.state.inputKey}
+            onChange={this.handleInputKeyChange}
+            style={{ width: '40%', height: 40 }}
+            placeholder="Key"
+          />
         </div>
 
-        <pre className="output"><code>{this.state.encryptedBase64Input}</code></pre>
+        <pre className="output">
+          <code>{this.state.encryptedBase64Input}</code>
+        </pre>
 
         <h1>Descriptar o Texto !</h1>
         <div className="form-group">
-          <input className="form-control" value={this.state.encryptedBase64} onChange={this.handleMsgChange} style={{width:'40%', height:40, marginRight: 20}} placeholder="Encrypted String" /> 
-          <input className="form-control" value={this.state.key} onChange={this.handleDecryptKeyChange} style={{width:'40%', height:40}} placeholder="Key" />
+          <input
+            className="form-control"
+            value={this.state.encryptedBase64}
+            onChange={this.handleMsgChange}
+            style={{ width: '40%', height: 40, marginRight: 20 }}
+            placeholder="Encrypted String"
+          />
+          <input
+            className="form-control"
+            value={this.state.key}
+            onChange={this.handleDecryptKeyChange}
+            style={{ width: '40%', height: 40 }}
+            placeholder="Key"
+          />
         </div>
       </>
     );
